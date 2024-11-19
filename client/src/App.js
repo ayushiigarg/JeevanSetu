c91410db7f67634d90e3678623b774322aecb8fc
Ayushi <ayushigarg2709@gmail.com>
2024-11-19 22:34:24 +0530

    Frontend setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
-import {Routes,Route} from 'react-router-dom'
+ import {Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import Register from './pages/auth/Register';
 import Login from './pages/auth/Login';
 function App() {
   return (
-    <div>
-       <Routes >
-          <Route path="/" element={<HomePage />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-       </Routes>
+    <div >
+      <Routes>  
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+
     </div>
   );
 }
--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import InputType from '../../components/shared/Helper/InputType';
+//import InputType from '../../components/shared/form/InputType';
 const Login = () => {
   return (
     <>
@@ -14,14 +15,14 @@ const Login = () => {
           </div>
 
           <form>
-            <div className="mb-3">
-              <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-            </div>
-
+          <InputType></InputType>
             <div className="mb-3">
               <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-              <input type="password" className="form-control" id="exampleInputPassword1" />
+              <input 
+                type="password" 
+                className="form-control" 
+                id="exampleInputPassword1" 
+              />
             </div>
 
             <button type="submit" className="btn btn-primary">Submit</button>
