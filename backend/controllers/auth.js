fe2972dcca9a6f0eda9dfce62e39cdb084373d36
Ayushi <ayushigarg2709@gmail.com>
2024-12-11 19:49:42 +0530

    authentication done

---
+++ backend/controllers/auth.js
@@ -0,0 +1,83 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// Register user
+const registerUser = async (req, res) => {
+  try {
+    const { email, password, name } = req.body; // Destructuring the body
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create a new user
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword, // Store the hashed password
+    });
+
+    await user.save();
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+// Login user
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Check if user exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: "You have not registered yet" });
+    }
+
+    // Compare password
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Credentials",
+      });
+    }
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d", // Token expiration time
+      }
+    );
+
+    // Send the token as a response
+    res.status(200).json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = { registerUser, loginUser };
